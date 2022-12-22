fa0e4d2f0e2b8c2cd7e572e1f52e0e476ac076bb
Craig Brozefsky <craig@red-bean.com>
2022-12-21 20:28:22 -0500

    use the oxalica overlay in naersk build

--- flake.nix
+++ flake.nix
@@ -11,38 +11,41 @@
   outputs = { self, flake-utils, naersk, nixpkgs, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        inherit (pkgs.stdenv) isDarwin isLinux;
         overlays = [ (import rust-overlay) ];
         pkgs = (import nixpkgs) {
           inherit system overlays;
         };
 
-        naersk' = pkgs.callPackage naersk {
-        };
-
-        myrust = pkgs.rust-bin.stable.latest.default.override {
+        myrust = pkgs.rust-bin.beta.latest.default.override {
           extensions = [ "rust-analyzer" ];
         };
+
+        # configure naersk to use our rust overlay
+        naersk' = pkgs.callPackage naersk {
+          cargo = myrust;
+          rustc = myrust;
+        };
+
+        appkit = if isDarwin then pkgs.darwin.apple_sdk.frameworks.AppKit else null;
 
       in
       with pkgs;
       {
 
+
         # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
+        packages.default = naersk'.buildPackage {
           buildInputs =  [
-            darwin.apple_sdk.frameworks.AppKit
-            rust-bin.stable.latest.default
+            appkit
           ];
           src = ./.;
         };
 
         # For `nix develop`:
-        myrust = rust-bin.stable.latest.default.override {
-              extensions = ["rust-analyzer"];
-            };
-        devShell = pkgs.mkShell {
+        devShells.default = pkgs.mkShell {
           buildInputs = [
-            darwin.apple_sdk.frameworks.AppKit
+            appkit
             myrust
           ];
         };
